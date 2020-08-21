1f0165ff203edac2bc9270f9f0171271650a9f91
Hfreitas <hfreitas86@gmail.com>
2020-08-20 23:47:31 -0300

    iniciando exercicio

---
+++ router.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+// acessando arquivos
+const fs = require('fs').promises;
+// tratando path dos arquivos
+const path = require('path');
+
+const readFileData = async () => {
+  try {
+    /* Lendo o arquivo simpsons.json de forma assícrona. __dirname refere-se ao path 
+    absoluto do diretório que contém o executável*/
+    // join output - ./simpsons.json (relative path)
+    const data = await fs.readFile(path.join(__dirname,'simpsons.json'))  
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+router.get('/', (_req, res) => {
+try {
+  const response = await readFileData();
+  res.status(200).send(response);
+} catch (error) {
+  res.status(401).send('Ooops!');
+}
+});
+
+module.exports = router;
